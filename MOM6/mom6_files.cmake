83a8083d3c82f2cc586bf5ccd1f17b6250436cd3
Micael Oliveira <micael.oliveira@anu.edu.au>
2023-04-04 14:57:27 +1000

    Several further changes to use the CESM driver:  - compile the corresponding MOM6 cap (this is simply a module wrapper around the real cap).  - add options to turn on/off the different components (MOM6, WW3 and CICE6).  - do not compile the data components when the corresponding model component is used.  - set the *_PRESENT compile definitions for the CESM driver.

--- MOM6/mom6_files.cmake
+++ MOM6/mom6_files.cmake
@@ -293,6 +293,7 @@ list(APPEND mom6_src_files
   MOM6/config_src/infra/FMS2/MOM_time_manager.F90
 
   MOM6/config_src/drivers/nuopc_cap/mom_cap.F90
+  MOM6/config_src/drivers/nuopc_cap/ocn_comp_NUOPC.F90
   MOM6/config_src/drivers/nuopc_cap/time_utils.F90
   MOM6/config_src/drivers/nuopc_cap/mom_cap_methods.F90
   MOM6/config_src/drivers/nuopc_cap/mom_cap_time.F90
