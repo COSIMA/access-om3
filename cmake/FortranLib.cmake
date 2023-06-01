5b7f47c8aff49dcf4bdc0eed97195eea5e79ec56
Micael Oliveira <micael.oliveira@anu.edu.au>
2023-06-01 10:57:07 +1000

    Reduce some of the CMake boiler-plate by adding a add_fortran_library function that takes care of setting the module path.

---
+++ cmake/FortranLib.cmake
@@ -0,0 +1,8 @@
+function(add_fortran_library LIB MOD_DIR)
+    add_library(${LIB} ${ARGN})
+
+    get_target_property(LIB_DIR ${LIB} BINARY_DIR)
+    set_target_properties(${LIB} PROPERTIES Fortran_MODULE_DIRECTORY ${LIB_DIR}/${MOD_DIR})
+
+    target_include_directories(${LIB} INTERFACE ${LIB_DIR}/${MOD_DIR})
+endfunction(add_fortran_library)
