8844c57e02588b7270eb5d4ffb5f5c1b30336ca4
Micael Oliveira <micael.oliveira@anu.edu.au>
2023-07-19 09:53:26 +1000

    Update build script to use latest spack environment.

--- build.sh
+++ build.sh
@@ -20,8 +20,8 @@ OPENMPI_VERSION=4.1.4
 
 module purge
 module load cmake/3.24.2
-module use /g/data/ik11/spack/0.19.0/share/modules/linux-rocky8-cascadelake  # requires membership of "ik11" group
-module load esmf/8.3.1-intel-${COMPILER_VERSION} fms/2020.04.03-intel-${COMPILER_VERSION} parallelio/2.5.9-intel-${COMPILER_VERSION}
+module use /g/data/ik11/spack/0.20.1/share/modules/linux-rocky8-cascadelake  # requires membership of "ik11" group
+module load esmf/8.3.1-intel-${COMPILER_VERSION} fms/2023.01-intel-${COMPILER_VERSION} parallelio/2.5.10-intel-${COMPILER_VERSION}
 module load intel-compiler/${COMPILER_VERSION} openmpi/${OPENMPI_VERSION}
 
 cd ${SCRIPT_DIR}
