27f95904c39cf072c379605ff66c5917695cd92e
Micael Oliveira <micael.oliveira@anu.edu.au>
2023-10-17 14:59:36 +1100

    Add installation of OM3 executables and WW3 utilities to CMakeLists. Update build script to use "cmake --install" and rename all executables.

--- build.sh
+++ build.sh
@@ -25,11 +25,10 @@ module load esmf/8.4.2 fms/git.2021.03.01=2021.03.01 parallelio/2.5.10
 module load intel-compiler/${COMPILER_VERSION} openmpi/${OPENMPI_VERSION}
 
 cd ${SCRIPT_DIR}
+INSTALL_DIR=${SCRIPT_DIR}
 
 hash=`git rev-parse --short=7 HEAD`
 test -z "$(git status --porcelain)" || hash=${hash}-modified # uncommitted changes or untracked files
-
-mkdir -p bin
 
 for BUILD_TYPE in "${BUILD_TYPES[@]}"; do
   echo "BUILD_TYPE = "${BUILD_TYPE}
@@ -37,12 +36,13 @@ for BUILD_TYPE in "${BUILD_TYPES[@]}"; do
 
   cmake -S . -B build --preset=gadi -DCMAKE_BUILD_TYPE=${BUILD_TYPE} -DCMAKE_VERBOSE_MAKEFILE=ON
   cmake --build build -j 4
+  cmake --install build --prefix=${INSTALL_DIR}
 
-  for exec in build/access-om3*; do
-    dest=bin/$(basename ${exec})
+  for exec in ${INSTALL_DIR}/bin/*; do
+    dest=${INSTALL_DIR}/bin/$(basename ${exec})
     if [[ ${BUILD_TYPE} != "Release" ]] ; then dest=${dest}-${BUILD_TYPE}; fi
     dest=${dest}-${hash}
-    cp -p ${exec} ${dest}
+    mv ${exec} ${dest}
     echo "Successfully built ${dest}"
   done
 done
